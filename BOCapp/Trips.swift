b1fa41c163199fe5f4a9c630d81697368db77d44
Alexander Shi <xander.shi@gmail.com>
2016-04-19 11:47:49 -0400

    Added trips class

---
+++ BOCapp/Trips.swift
@@ -0,0 +1,16 @@
+//
+//  Trips.swift
+//  BOCapp
+//
+//  Created by Alexander Shi on 4/19/16.
+//  Copyright © 2016 Alexander Shi. All rights reserved.
+//
+
+import Foundation
+
+class Trips {
+    
+    
+    
+    
+}
