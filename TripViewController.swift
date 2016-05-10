ba70fc8c61751ff0c06efd2a836b110498987ff5
Andrew Murowchick <amurow9@gmail.com>
2016-05-10 14:11:32 -0400

    added tableview for tripviewcontroller

--- TripViewController.swift
+++ TripViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TripViewController: UIViewController { //, TripViewDataSource {
+class TripViewController: UIViewController, UITableViewDelegate { //, TripViewDataSource {
  
     
     // MARK: Properties for trip view
@@ -19,6 +19,7 @@ class TripViewController: UIViewController { //, TripViewDataSource {
     @IBOutlet weak var leaderName: PaddingLabel!
     @IBOutlet weak var leaderPicture: UIImageView!
     @IBOutlet weak var tripCapacity: UILabel!
+    @IBOutlet weak var trippersTable: UITableView!
     
 //    @IBOutlet weak var tripView: TripView! {
 //        didSet {
@@ -35,6 +36,30 @@ class TripViewController: UIViewController { //, TripViewDataSource {
     @IBOutlet weak var tripView: TripView!  // UIScrollView 
     var trip: Trip? // Pushed over by segue when selected from TableView
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let currentTrip = trip
+        return currentTrip!.tripMembers.count
+    }
+    
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let tripCellIdentifier = "TrippersTableViewCell"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(tripCellIdentifier, forIndexPath: indexPath) as! TrippersTableViewCell
+        
+        let currentTrip = trip
+        
+        // Load data to individual elements of TripTableViewCell
+        cell.tripperName.text = currentTrip!.tripMembers[indexPath.row].firstName + " " + currentTrip!.tripMembers[indexPath.row].lastName
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,6 +73,8 @@ class TripViewController: UIViewController { //, TripViewDataSource {
             leaderName.text = trip.leaderName
             leaderPicture.image = trip.leaderImage
             tripCapacity.text = String(10 - trip.tripMembers.count) + "/10 spots available"
+            
+            
             
             
             tripName.sizeToFit()
